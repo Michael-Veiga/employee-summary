074db4152c12318a85afd60623f648de748b6b27
Mike V <mvee333@gmail.com>
2020-06-04 11:39:08 -0400

    called addEmployee and altered variables

--- Develop/app.js
+++ Develop/app.js
@@ -10,14 +10,104 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+function questions() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Whats your name?",
+      name: "name",
+    },
+    {
+      type: "number",
+      message: "Whats your ID number?",
+      name: "ID",
+    },
+    {
+      type: "input",
+      message: "Whats your email address?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "Whats your job title?",
+      name: "job",
+      choices: ["Manager", "Engineer", "Intern"]
+    },
+  ]).then(
+      // and to create objects for each team member (using the correct classes as blueprints!)
+      function({name, ID, email, job}) {
+        switch(job) {
+            case "Manager":
+                inquirer
+                    .prompt({
+                        type: "input",
+                        message: "What is your office number??",
+                        name: "officeNumber",
+                    }).then(
+                        function ({officeNumber}) {
+                            Manager(name, ID, email, officeNumber)
+                            addEmployee()
+                        }
+                    )
+                    break
+            case "Intern":
+                   inquirer
+                        .prompt({
+                            type: "input",
+                            message: "What school did you attend?",
+                            name: "school",
+                        }).then(
+                            function ({school}) {
+                                Intern(name, id, email, school)
+                                addEmployee()
+                            }
+                        )
+                        break
+                    case "Engineer":
+                        inquirer
+                            .prompt({
+                                type: "input",
+                                message: "What is your github username?",
+                                name:"github"
+                            }).then(
+                                function ({github}) {
+                                    Engineer(name, id, email, github)
+                                    addEmployee()
+                                }
+                            )
+                            break
+                }
+      })
+  
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+function addEmployee {
+    inquirer.prompt({
+        type: "confirm",
+        message: "Would you like to add another employee?",
+        name: "addEmployee"
+    }).then(
+        function({addEmployee}) {
+            console.log("add another employee", addEmployee)
+            if (addEmployee) {
+                questions()
+            } else {
+                render()
+            } 
+        }
+    )
+
+    .catch(err => {
+        console.log("Error adding employee", err)
+        throw err
+    })
+
+}
+questions()
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
